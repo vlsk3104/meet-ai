ac14abf739afc14a737d0ecb4274f79cd1671fe7
vlsk3104 <vlsk3104@gmail.com>
2025-05-31 11:53:21 +0900

    wip

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,4 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
 const sql = neon(process.env.DATABASE_URL!);
-export const database = drizzle(sql);
+export const db = drizzle(sql);
