0f27135bb5ac17a4290b081a06f52f8c2f922339
vlsk3104 <vlsk3104@gmail.com>
2025-05-31 11:44:06 +0900

    database

---
+++ src/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+	id: integer("id").primaryKey().notNull(),
+	name: varchar("name", { length: 255 }).notNull(),
+	age: integer("age").notNull(),
+	email: varchar("email", { length: 255 }).notNull().unique(),
+});
